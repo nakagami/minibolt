7acbca35857f47a4e00a7367507fb63c63c78faa
Hajime Nakagami <nakagami@gmail.com>
2017-09-20 22:42:10 +0900

    revision number up

--- minibolt.py
+++ minibolt.py
@@ -28,7 +28,7 @@ except ImportError:
     import usocket as socket
     import ustruct as struct
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __all__ = ('connect', 'to_nxgraph', 'nodes', 'relationships')
 
 
